607c516606ce4fda2a3edfd0dcdf290c943352d8
David Edwards <david.edwr@gmail.com>
2015-04-18 11:42:34 -0400

    Add watchify to build js on changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
+var watchify = require('watchify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+var gutil = require('gulp-util');
 
-gulp.task('js', function () {
-  return browserify('./js/cheshire.js')
-    .bundle()
+var watcherOptions = watchify.args;
+watcherOptions.entries = ['./js/cheshire.js'];
+var builder = watchify(browserify(watcherOptions));
+
+gulp.task('js', bundle);
+builder.on('update', bundle);
+builder.on('log', gutil.log);
+
+gulp.task('default', ['js']);
+
+function bundle() {
+  return builder.bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('./dist/'));
-});
-
-gulp.task('default', function() {
-  // place code for your default task here
-});
+}
